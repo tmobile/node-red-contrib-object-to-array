83f2ab6aabb614ab79299464dc5003ec695ed0b0
James Tharpe <james.tharpe1@t-mobile.com>
2020-11-02 09:35:48 -0500

    Add code headers for license compliance

--- src/object-to-array.spec.ts
+++ src/object-to-array.spec.ts
@@ -1,3 +1,21 @@
+/**
+Copyright 2020 T-Mobile USA, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+See the LICENSE file for additional language around the disclaimer of warranties.
+Trademark Disclaimer: Neither the name of “T-Mobile, USA” nor the names of
+its contributors may be used to endorse or promote products
+*/
+
 import objectToArray from "./object-to-array";
 import helper from "node-red-node-test-helper";
 import { expect } from "chai";
@@ -16,9 +34,9 @@ describe("object-to-array Node", function () {
   });
 
   it("should be loaded", function (done) {
-    var flow = [{ id: "n1", type: "object-to-array", name: "test name" }];
+    const flow = [{ id: "n1", type: "object-to-array", name: "test name" }];
     helper.load(objectToArray, flow, function () {
-      var n1 = helper.getNode("n1");
+      const n1 = helper.getNode("n1");
       expect(n1.name).to.equal("test name");
       done();
     });
@@ -26,7 +44,7 @@ describe("object-to-array Node", function () {
 
   it("should transform payload", function (done) {
     const payload = {"apple": 1, "banana": 2};
-    var flow = [
+    const flow = [
       {
         id: "n1",
         type: "object-to-array",
